6fe8c31562b57f2900da659a60798ac77e3911d6
Christian Vieux <christianvieux.dev@gmail.com>
2025-06-16 15:12:31 -0400

    Update heroContent.js
    new profile img

--- src/app/data/heroContent.js
+++ src/app/data/heroContent.js
@@ -2,7 +2,7 @@
 export const heroContent = {
   title: "Junior Software Engineer",
   image: {
-    src: "https://res.cloudinary.com/dqbydfphk/image/upload/v1739083627/profile_fbibnp.jpg",
+    src: "https://res.cloudinary.com/dqbydfphk/image/upload/v1750100944/Portfolio/ax9lygypvqjclhurjnfo.jpg",
     alt: "Christian Vieux",
   },
   tagline: `Building full-stack web apps with a focus on **backend development** and **cloud solutions**. I enjoy working with **modern tools** and writing code that’s clear, reliable, and easy to build on.`,

e2f63b4ccbc774269e56ae5651ad763b81638b2c
Christian Vieux <christianvieux.dev@gmail.com>
2025-06-21 04:26:41 -0400

    Update aboutContent.js

--- src/app/data/aboutContent.js
+++ src/app/data/aboutContent.js
@@ -3,9 +3,9 @@ export const aboutContent = {
   mainDescription: `
   Hi, I’m Christian — a junior software engineer with a background in game development and a passion for building things that people can use and enjoy.
 
-  My coding journey started back in 2014 when I was in high school, developing games on Roblox just for fun. I loved creating worlds, learning new tools, and collaborating with others — and over time, that hobby turned into something more serious. About two years ago, I started diving into web development. I began with a Udemy course, then challenged myself to build a full project from scratch, which really leveled me up. That experience pushed me to enroll in General Assembly’s Software Engineering Bootcamp, where I deepened my technical knowledge, grew my soft skills, and connected with some amazing people and mentors.
+  My coding journey started back in 2014 when I was in high school, developing games on Roblox just for fun. I loved creating worlds, learning new tools, and collaborating with others and over time, that hobby turned into something more serious. About two years ago, I started diving into web development. I began with a Udemy course, then challenged myself to build a full project from scratch, which really leveled me up. That experience pushed me to enroll in General Assembly’s Software Engineering Bootcamp, where I deepened my technical knowledge, grew my soft skills, and connected with some amazing people and mentors.
 
-  Now I’m looking for my first junior software engineering role — not just to get a job, but to join a team where I can grow, contribute, and make a real impact. I’m not claiming to be a unicorn or the best in the room, but I care deeply about what I do. When I’m passionate about something, I go all in — and I’m ready to bring that energy to the right team.
+  Now I’m looking for my first junior software engineering role but not just to get a job, but to join a team where I can grow, contribute, and make a real impact. I’m not claiming to be a unicorn or the best in the room, but I care deeply about what I do. When I’m passionate about something, I go all in! and I’m ready to bring that energy to the right team.
 
   Thanks for taking the time to learn a bit about me. I’m excited for what’s ahead and hopeful to work with anyone who believe in giving people a real shot.
   `,
